7cb3acabbec44f23680ac3711e1c1aaa9c6116c7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-12-25 11:09:55 +0100

    switch between SimpleList and DataGrid according to breakpoint

--- src/users.tsx
+++ src/users.tsx
@@ -1,18 +1,28 @@
-// import { List, Datagrid, TextField, EmailField } from 'react-admin'
-import { List, SimpleList } from 'react-admin'
+import { useMediaQuery, Theme } from "@mui/material";
+import { List, Datagrid, TextField, EmailField, SimpleList } from "react-admin";
 
-export const UsersList = () => (
-  <List>
-    {/* <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="username" />
-      <EmailField source='email' />
-      <TextField source="address.street" />
-      <TextField source="phone" />
-      <TextField source="website" />
-      <TextField source="company.name" />
-    </Datagrid> */}
-    <SimpleList primaryText={(record) => record.name} secondaryText={(record) => record.username} tertiaryText={(record) => record.email} />
-  </List>
-)
+export const UsersList = () => {
+  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+  return (
+    <List>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.name}
+          secondaryText={(record) => record.username}
+          tertiaryText={(record) => record.email}
+        />
+      ) : (
+        <Datagrid rowClick="edit">
+          <TextField source="id" />
+          <TextField source="name" />
+          <TextField source="username" />
+          <EmailField source="email" />
+          <TextField source="address.street" />
+          <TextField source="phone" />
+          <TextField source="website" />
+          <TextField source="company.name" />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
