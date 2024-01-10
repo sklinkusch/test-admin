1e74b365d3c91349d0008854f4e6352519d17701
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-01-10 09:27:25 +0100

    use MyUrlField in UsersList

--- src/users.tsx
+++ src/users.tsx
@@ -1,12 +1,6 @@
 import { useMediaQuery, Theme } from "@mui/material";
-import {
-  List,
-  SimpleList,
-  Datagrid,
-  TextField,
-  EmailField,
-  UrlField
-} from "react-admin";
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
 
 export const UsersList = () => {
   const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
@@ -24,7 +18,7 @@ export const UsersList = () => {
           <TextField source="name" />
           <EmailField source="email" />
           <TextField source="phone" />
-          <UrlField source="website" />
+          <MyUrlField source="website" />
           <TextField source="company.name" />
         </Datagrid>
       )}
