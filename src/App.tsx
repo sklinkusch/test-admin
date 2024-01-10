83cf55d56fb32f3ef19063c21f30ef0ab7c470f7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-01-10 10:30:13 +0100

    show also posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import {
-  Admin,
-  Resource
-} from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UsersList } from "./users";
 
 export const App = () => (
-<Admin dataProvider={dataProvider}>
-  <Resource name="users" list={UsersList} />
-</Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="users" list={UsersList} />
+  </Admin>
 );
