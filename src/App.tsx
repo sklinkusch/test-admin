0cf74d7f8c3d41d2097a914a80c4c7dd200f98c2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-01-15 20:16:59 +0100

    add event listener on storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { Admin, Resource, ListGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UsersList } from "./users";
+import { useState, useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
 
-export const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
-    <Resource name="users" list={UsersList} />
-  </Admin>
-);
+type Listener = void | null;
+type Mode = "dark" | "light";
+
+export const App = () => {
+  const [listener, setListener] = useState<Listener>(null);
+  const [mode, setMode] = useState<Mode>("dark");
+  useEffect(() => {
+    const myListener = window.addEventListener("storage", () => {
+      setMode(localStorage.getItem("mode") == "1" ? "dark" : "light");
+    });
+    setListener(myListener);
+    const removeListener = (theListener: any) => {
+      window.removeEventListener("storage", theListener);
+    };
+    return removeListener(listener);
+  }, []);
+  return (
+    <Admin dataProvider={dataProvider} defaultTheme={mode}>
+      <Resource name="posts" list={ListGuesser} />
+      <Resource name="users" list={UsersList} />
+    </Admin>
+  );
+};
