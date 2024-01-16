41b0bd7b9f4416ca8278c8525ce525fb86d4b7dd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-01-16 04:59:11 +0100

    a few comments on the code

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { dataProvider } from "./dataProvider";
 import { UsersList } from "./users";
 import { useState, useEffect } from "react";
 import { ThemeOptions } from "@mui/material/styles";
-/* eslint-disable react-hooks/exhaustive-deps */
 
+// some types
 type Listener = void | null;
 type Mode = "dark" | "light";
 
+// basic definition of themes
 const darkTheme: ThemeOptions = {
   palette: {
     mode: "dark",
@@ -33,17 +34,25 @@ const lightTheme: ThemeOptions = {
 };
 
 export const App = () => {
+  // states for the mode and the eventListener
   const [listener, setListener] = useState<Listener>(null);
   const [mode, setMode] = useState<Mode>("dark");
   useEffect(() => {
+    // define new event listener on local storage
     const myListener = window.addEventListener("storage", () => {
       setMode(localStorage.getItem("mode") == "1" ? "dark" : "light");
     });
+    // store event listener in the respective state hook
     setListener(myListener);
+    // function to remove event listener
     const removeListener = (theListener: any) => {
       window.removeEventListener("storage", theListener);
     };
+    // return the remove function -> event listener is removed before the component App is unmounted
     return removeListener(listener);
+    // empty array at the end of useEffect -> useEffect callback is executed once after mounting component App
+    // eslint disable command to get rid of eslint warning for missing dependencies
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, []);
   return (
     <Admin
